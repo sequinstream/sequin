68c972b3eea703a610b37edab4ea1fe42cea0b3e
RTLS <carter.e.pedersen@gmail.com>
2024-07-23 10:01:00 -0700

    ♻️  Refactor with channel.go

--- cli/api/webhook_channel.go
+++ cli/api/webhook_channel.go
@@ -2,63 +2,29 @@ package api
 
 import (
 	"log"
-	"net/url"
 
-	"github.com/nshafer/phx"
 	"github.com/sequinstream/sequin/cli/context"
 )
 
 type WebhookChannel struct {
-	socket  *phx.Socket
-	channel *phx.Channel
+	BaseChannel
 }
 
 func NewWebhookChannel(ctx *context.Context) (*WebhookChannel, error) {
-	endPoint, err := url.Parse(ctx.WebSocketURL)
+	baseChannel, err := NewBaseChannel(ctx, "webhook", &SilentLogger{})
 	if err != nil {
 		return nil, err
 	}
 
-	socket := phx.NewSocket(endPoint)
-	socket.Logger = &silentLogger{}
-
 	wc := &WebhookChannel{
-		socket: socket,
+		BaseChannel: *baseChannel,
 	}
 
 	return wc, nil
 }
 
 func (wc *WebhookChannel) Connect() error {
-	err := wc.socket.Connect()
-	if err != nil {
-		return err
-	}
-
-	// Wait for the socket to connect
-	cont := make(chan bool)
-	wc.socket.OnOpen(func() {
-		cont <- true
-	})
-	<-cont
-
-	// Join the "webhook" channel
-	wc.channel = wc.socket.Channel("webhook", nil)
-	join, err := wc.channel.Join()
-	if err != nil {
-		return err
-	}
-
-	// Wait for join confirmation
-	join.Receive("ok", func(response any) {
-		cont <- true
-	})
-	join.Receive("error", func(response any) {
-		log.Println("Join error", response)
-	})
-	<-cont
-
-	return nil
+	return wc.BaseChannel.Connect("webhook")
 }
 
 func (wc *WebhookChannel) OnWebhookIngested(handler func(Webhook, Message)) {
@@ -74,12 +40,3 @@ func (wc *WebhookChannel) OnWebhookIngested(handler func(Webhook, Message)) {
 		handler(webhookPayload.Webhook, webhookPayload.Message)
 	})
 }
-
-// Reuse the parsePayload function from observe_channel.go
-// func parsePayload(payload any, target interface{}) error { ... }
-
-type silentLogger struct{}
-
-func (l *silentLogger) Print(_ phx.LoggerLevel, _ string, _ ...any)            {}
-func (l *silentLogger) Println(_ phx.LoggerLevel, _ string, _ ...any)          {}
-func (l *silentLogger) Printf(_ phx.LoggerLevel, _ string, _ string, _ ...any) {}

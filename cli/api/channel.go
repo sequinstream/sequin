95cf363f0923d537776a250f6d7b4362b8706c0f
RTLS <carter.e.pedersen@gmail.com>
2024-07-23 10:01:00 -0700

    ♻️  Refactor with channel.go

---
+++ cli/api/channel.go
@@ -0,0 +1,81 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/url"
+
+	"github.com/nshafer/phx"
+	"github.com/sequinstream/sequin/cli/context"
+)
+
+type BaseChannel struct {
+	socket  *phx.Socket
+	channel *phx.Channel
+}
+
+func NewBaseChannel(ctx *context.Context, channelName string, logger phx.Logger) (*BaseChannel, error) {
+	endPoint, err := url.Parse(ctx.WebSocketURL)
+	if err != nil {
+		return nil, err
+	}
+
+	socket := phx.NewSocket(endPoint)
+	socket.Logger = logger
+
+	bc := &BaseChannel{
+		socket: socket,
+	}
+
+	return bc, nil
+}
+
+func (bc *BaseChannel) Connect(channelName string) error {
+	err := bc.socket.Connect()
+	if err != nil {
+		return err
+	}
+
+	// Wait for the socket to connect
+	cont := make(chan bool)
+	bc.socket.OnOpen(func() {
+		cont <- true
+	})
+	<-cont
+
+	// Join the channel
+	bc.channel = bc.socket.Channel(channelName, nil)
+	join, err := bc.channel.Join()
+	if err != nil {
+		return err
+	}
+
+	// Wait for join confirmation
+	join.Receive("ok", func(response any) {
+		cont <- true
+	})
+	join.Receive("error", func(response any) {
+		log.Println("Join error", response)
+	})
+	<-cont
+
+	return nil
+}
+
+func parsePayload(payload any, target interface{}) error {
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("error marshaling payload: %w", err)
+	}
+	if err := json.Unmarshal(jsonData, target); err != nil {
+		return fmt.Errorf("error unmarshaling payload: %w", err)
+	}
+	return nil
+}
+
+type SilentLogger struct{}
+
+func (l *SilentLogger) Print(_ phx.LoggerLevel, _ string, _ ...any)            {}
+func (l *SilentLogger) Println(_ phx.LoggerLevel, _ string, _ ...any)          {}
+func (l *SilentLogger) Printf(_ phx.LoggerLevel, _ string, _ string, _ ...any) {}
--- cli/api/observe_channel.go
+++ cli/api/observe_channel.go
@@ -1,67 +1,30 @@
 package api
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
-	"net/url"
 
-	"github.com/nshafer/phx"
 	"github.com/sequinstream/sequin/cli/context"
 )
 
 type ObserveChannel struct {
-	socket  *phx.Socket
-	channel *phx.Channel
+	BaseChannel
 }
 
 func NewObserveChannel(ctx *context.Context) (*ObserveChannel, error) {
-	endPoint, err := url.Parse(ctx.WebSocketURL)
+	baseChannel, err := NewBaseChannel(ctx, "observe", &SilentLogger{})
 	if err != nil {
 		return nil, err
 	}
 
-	socket := phx.NewSocket(endPoint)
-	socket.Logger = phx.NewSimpleLogger(phx.LogInfo)
-
 	oc := &ObserveChannel{
-		socket: socket,
+		BaseChannel: *baseChannel,
 	}
 
 	return oc, nil
 }
 
 func (oc *ObserveChannel) Connect() error {
-	err := oc.socket.Connect()
-	if err != nil {
-		return err
-	}
-
-	// Wait for the socket to connect
-	cont := make(chan bool)
-	oc.socket.OnOpen(func() {
-		cont <- true
-	})
-	<-cont
-
-	// Join the "observe" channel
-	oc.channel = oc.socket.Channel("observe", nil)
-	join, err := oc.channel.Join()
-	if err != nil {
-		return err
-	}
-
-	// Wait for join confirmation
-	join.Receive("ok", func(response any) {
-		log.Println("Joined channel:", oc.channel.Topic(), response)
-		cont <- true
-	})
-	join.Receive("error", func(response any) {
-		log.Println("Join error", response)
-	})
-	<-cont
-
-	return nil
+	return oc.BaseChannel.Connect("observe")
 }
 
 func (oc *ObserveChannel) OnStreamCreated(handler func(Stream)) {
@@ -144,15 +107,4 @@ func (oc *ObserveChannel) OnMessagesUpserted(handler func([]Message)) {
 
 		handler(tp.Messages)
 	})
-}
-
-func parsePayload(payload any, target interface{}) error {
-	jsonData, err := json.Marshal(payload)
-	if err != nil {
-		return fmt.Errorf("error marshaling payload: %w", err)
-	}
-	if err := json.Unmarshal(jsonData, target); err != nil {
-		return fmt.Errorf("error unmarshaling payload: %w", err)
-	}
-	return nil
 }

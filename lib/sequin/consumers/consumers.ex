40f001315c9720c62e3619612bdac5dc287af2c8
Anthony Accomazzo <accomazz@gmail.com>
2024-08-16 11:25:44 -0700

    Reorganize Consumers context

--- lib/sequin/consumers/consumers.ex
+++ lib/sequin/consumers/consumers.ex
@@ -22,47 +22,6 @@ defmodule Sequin.Consumers do
     |> Repo.one()
   end
 
-  def list_consumer_events_for_consumer(consumer_id, params \\ []) do
-    base_query = ConsumerEvent.where_consumer_id(consumer_id)
-
-    query =
-      Enum.reduce(params, base_query, fn
-        {:is_deliverable, false}, query ->
-          ConsumerEvent.where_not_visible(query)
-
-        {:is_deliverable, true}, query ->
-          ConsumerEvent.where_deliverable(query)
-
-        {:limit, limit}, query ->
-          limit(query, ^limit)
-
-        {:order_by, order_by}, query ->
-          order_by(query, ^order_by)
-      end)
-
-    Repo.all(query)
-  end
-
-  def get_consumer_event(consumer_id, commit_lsn) do
-    consumer_event =
-      consumer_id
-      |> ConsumerEvent.where_consumer_id()
-      |> ConsumerEvent.where_commit_lsn(commit_lsn)
-      |> Repo.one()
-
-    case consumer_event do
-      nil -> {:error, Error.not_found(entity: :consumer_event)}
-      consumer_event -> {:ok, consumer_event}
-    end
-  end
-
-  def get_consumer_event!(consumer_id, commit_lsn) do
-    case get_consumer_event(consumer_id, commit_lsn) do
-      {:ok, consumer_event} -> consumer_event
-      {:error, _} -> raise Error.not_found(entity: :consumer_event)
-    end
-  end
-
   @stream_schema Application.compile_env!(:sequin, [Sequin.Repo, :stream_schema_prefix])
   @config_schema Application.compile_env!(:sequin, [Sequin.Repo, :config_schema_prefix])
 
@@ -276,6 +235,75 @@ defmodule Sequin.Consumers do
     {:ok, events}
   end
 
+  @spec ack_event_messages(Sequin.Consumers.Consumer.t(), [integer()]) :: :ok
+  def ack_event_messages(%Consumer{} = consumer, commit_lsns) do
+    Repo.transact(fn ->
+      {_, _} =
+        consumer.id
+        |> ConsumerEvent.where_consumer_id()
+        |> ConsumerEvent.where_commit_lsns(commit_lsns)
+        |> Repo.delete_all()
+
+      :ok
+    end)
+
+    :ok
+  end
+
+  @spec nack_event_messages(Sequin.Consumers.Consumer.t(), [integer()]) :: :ok
+  def nack_event_messages(%Consumer{} = consumer, commit_lsns) do
+    {_, _} =
+      consumer.id
+      |> ConsumerEvent.where_consumer_id()
+      |> ConsumerEvent.where_commit_lsns(commit_lsns)
+      |> Repo.update_all(set: [not_visible_until: nil])
+
+    :ok
+  end
+
+  # ConsumerEvent
+
+  def list_consumer_events_for_consumer(consumer_id, params \\ []) do
+    base_query = ConsumerEvent.where_consumer_id(consumer_id)
+
+    query =
+      Enum.reduce(params, base_query, fn
+        {:is_deliverable, false}, query ->
+          ConsumerEvent.where_not_visible(query)
+
+        {:is_deliverable, true}, query ->
+          ConsumerEvent.where_deliverable(query)
+
+        {:limit, limit}, query ->
+          limit(query, ^limit)
+
+        {:order_by, order_by}, query ->
+          order_by(query, ^order_by)
+      end)
+
+    Repo.all(query)
+  end
+
+  def get_consumer_event(consumer_id, commit_lsn) do
+    consumer_event =
+      consumer_id
+      |> ConsumerEvent.where_consumer_id()
+      |> ConsumerEvent.where_commit_lsn(commit_lsn)
+      |> Repo.one()
+
+    case consumer_event do
+      nil -> {:error, Error.not_found(entity: :consumer_event)}
+      consumer_event -> {:ok, consumer_event}
+    end
+  end
+
+  def get_consumer_event!(consumer_id, commit_lsn) do
+    case get_consumer_event(consumer_id, commit_lsn) do
+      {:ok, consumer_event} -> consumer_event
+      {:error, _} -> raise Error.not_found(entity: :consumer_event)
+    end
+  end
+
   def insert_consumer_events([]), do: {:ok, []}
 
   def insert_consumer_events(consumer_events) do
@@ -292,31 +320,7 @@ defmodule Sequin.Consumers do
     {:ok, count}
   end
 
-  @spec ack_event_messages(Sequin.Streams.Consumer.t(), [integer()]) :: :ok
-  def ack_event_messages(%Consumer{} = consumer, commit_lsns) do
-    Repo.transact(fn ->
-      {_, _} =
-        consumer.id
-        |> ConsumerEvent.where_consumer_id()
-        |> ConsumerEvent.where_commit_lsns(commit_lsns)
-        |> Repo.delete_all()
-
-      :ok
-    end)
-
-    :ok
-  end
-
-  @spec nack_event_messages(Sequin.Streams.Consumer.t(), [integer()]) :: :ok
-  def nack_event_messages(%Consumer{} = consumer, commit_lsns) do
-    {_, _} =
-      consumer.id
-      |> ConsumerEvent.where_consumer_id()
-      |> ConsumerEvent.where_commit_lsns(commit_lsns)
-      |> Repo.update_all(set: [not_visible_until: nil])
-
-    :ok
-  end
+  # Consumer Backfills
 
   def backfill_limit, do: 10_000
 

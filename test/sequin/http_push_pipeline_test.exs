93a13ee46af6e0fe43f2c8d00f2d0c2f10a821bb
Anthony Accomazzo <accomazz@gmail.com>
2024-08-16 19:07:51 -0700

    Tests working for HttpEndpoint

--- test/sequin/http_push_pipeline_test.exs
+++ test/sequin/http_push_pipeline_test.exs
@@ -3,66 +3,66 @@ defmodule Sequin.StreamsRuntime.HttpPushPipelineTest do
 
   alias Sequin.Factory.AccountsFactory
   alias Sequin.Factory.ConsumersFactory
-  alias Sequin.Factory.StreamsFactory
   alias Sequin.StreamsRuntime.HttpPushPipeline
-
-  @moduletag skip: true
 
   setup do
     account = AccountsFactory.insert_account!()
-    http_endpoint = StreamsFactory.insert_http_endpoint!(account_id: account.id)
+    http_endpoint = ConsumersFactory.insert_http_endpoint!(account_id: account.id)
 
     consumer =
-      [account_id: account.id, http_endpoint_id: http_endpoint.id, kind: :push]
-      |> ConsumersFactory.insert_consumer!()
+      [account_id: account.id, http_endpoint_id: http_endpoint.id, message_kind: :event]
+      |> ConsumersFactory.insert_http_push_consumer!()
       |> Repo.preload(:http_endpoint)
 
     {:ok, %{consumer: consumer, http_endpoint: http_endpoint}}
   end
 
-  test "messages are sent to the HTTP endpoint", %{consumer: consumer, http_endpoint: http_endpoint} do
+  test "events are sent to the HTTP endpoint", %{consumer: consumer, http_endpoint: http_endpoint} do
     test_pid = self()
-    message = StreamsFactory.insert_message!(stream_id: consumer.stream_id)
+    event = ConsumersFactory.insert_consumer_event!(consumer_id: consumer.id, action: :insert)
 
     adapter = fn %Req.Request{} = req ->
       assert to_string(req.url) == http_endpoint.base_url
       json = Jason.decode!(req.body)
-      assert json["data"] == message.data
-      assert json["key"] == message.key
+
+      assert json == %{
+               "record" => event.data["record"],
+               "changes" => nil,
+               "action" => "insert"
+             }
+
       send(test_pid, :sent)
       {req, Req.Response.new(status: 200)}
     end
 
     start_pipeline!(consumer, adapter)
 
-    ref = send_test_message(consumer, message)
-    assert_receive {:ack, ^ref, [%{data: msg}], []}, 1_000
-    assert is_struct(msg, Sequin.Streams.Message)
-    assert msg.key == message.key
+    ref = send_test_event(consumer, event.data)
+    assert_receive {:ack, ^ref, [%{data: %{"action" => :insert}}], []}, 1_000
     assert_receive :sent, 1_000
   end
 
   @tag capture_log: true
-  test "a non-200 response results in a failed message", %{consumer: consumer} do
+  test "a non-200 response results in a failed event", %{consumer: consumer} do
     adapter = fn req ->
       {req, Req.Response.new(status: 500)}
     end
 
     start_pipeline!(consumer, adapter)
 
-    ref = send_test_message(consumer)
+    ref = send_test_event(consumer)
     assert_receive {:ack, ^ref, [], [_failed]}, 2_000
   end
 
   @tag capture_log: true
-  test "a transport error/timeout results in a failed message", %{consumer: consumer} do
+  test "a transport error/timeout results in a failed event", %{consumer: consumer} do
     adapter = fn req ->
       {req, %Mint.TransportError{reason: :timeout}}
     end
 
     start_pipeline!(consumer, adapter)
 
-    ref = send_test_message(consumer)
+    ref = send_test_event(consumer)
     assert_receive {:ack, ^ref, [], [_failed]}, 2_000
   end
 
@@ -73,9 +73,9 @@ defmodule Sequin.StreamsRuntime.HttpPushPipelineTest do
     )
   end
 
-  defp send_test_message(consumer, message \\ nil) do
-    message = message || StreamsFactory.insert_message!(stream_id: consumer.stream_id)
-    Broadway.test_message(broadway(consumer), message, metadata: %{topic: "test_topic", headers: []})
+  defp send_test_event(consumer, event \\ nil) do
+    event = event || ConsumersFactory.insert_consumer_event!(consumer_id: consumer.id).data
+    Broadway.test_message(broadway(consumer), event, metadata: %{topic: "test_topic", headers: []})
   end
 
   defp broadway(consumer) do

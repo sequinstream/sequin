24a0b60b2517180fe6d11b652136e7859d2404ac
Anthony Accomazzo <accomazz@gmail.com>
2024-08-17 09:39:57 -0700

    Restore pull_controller test

--- test/sequin_web/controllers/pull_controller_test.exs
+++ test/sequin_web/controllers/pull_controller_test.exs
@@ -1,185 +1,153 @@
 defmodule SequinWeb.PullControllerTest do
   use SequinWeb.ConnCase, async: true
 
+  alias Sequin.Consumers
   alias Sequin.Factory.AccountsFactory
   alias Sequin.Factory.ConsumersFactory
-  alias Sequin.Factory.StreamsFactory
-  alias Sequin.Streams
 
   setup :authenticated_conn
 
-  @moduletag skip: true
   @one_day_ago DateTime.add(DateTime.utc_now(), -24, :hour)
 
   setup %{account: account} do
     other_account = AccountsFactory.insert_account!()
-    stream = StreamsFactory.insert_stream!(account_id: account.id)
-    other_stream = StreamsFactory.insert_stream!(account_id: other_account.id)
 
     consumer =
-      ConsumersFactory.insert_consumer!(account_id: account.id, stream_id: stream.id, backfill_completed_at: @one_day_ago)
+      ConsumersFactory.insert_http_pull_consumer!(
+        message_kind: :event,
+        account_id: account.id,
+        backfill_completed_at: @one_day_ago
+      )
 
-    other_consumer = ConsumersFactory.insert_consumer!(account_id: other_account.id, stream_id: other_stream.id)
-    %{stream: stream, consumer: consumer, other_consumer: other_consumer, other_stream: other_stream}
+    other_consumer = ConsumersFactory.insert_http_pull_consumer!(message_kind: :event, account_id: other_account.id)
+    %{consumer: consumer, other_consumer: other_consumer}
   end
 
   describe "receive" do
     test "returns 404 if trying to pull for another account's consumer", %{
       conn: conn,
-      other_consumer: other_consumer,
-      other_stream: other_stream
+      other_consumer: other_consumer
     } do
-      conn = get(conn, ~p"/api/streams/#{other_stream.id}/consumers/#{other_consumer.id}/receive")
+      conn = get(conn, ~p"/api/http_pull_consumers/#{other_consumer.id}/receive")
       assert json_response(conn, 404)
     end
 
-    test "returns empty list if no ConsumerMessages to return", %{conn: conn, consumer: consumer, stream: stream} do
-      conn = get(conn, ~p"/api/streams/#{stream.id}/consumers/#{consumer.id}/receive")
+    test "returns empty list if no messages to return", %{conn: conn, consumer: consumer} do
+      conn = get(conn, ~p"/api/http_pull_consumers/#{consumer.id}/receive")
       assert %{"data" => []} = json_response(conn, 200)
     end
 
-    test "returns available messages if mix of available and delivered", %{conn: conn, consumer: consumer, stream: stream} do
-      available_message = StreamsFactory.insert_message!(%{stream_id: stream.id})
-      delivered_message = StreamsFactory.insert_message!(%{stream_id: stream.id})
+    test "returns available messages if mix of available and delivered", %{conn: conn, consumer: consumer} do
+      event = ConsumersFactory.insert_consumer_event!(consumer_id: consumer.id, not_visible_until: nil)
 
-      cm =
-        StreamsFactory.insert_consumer_message!(%{
-          consumer_id: consumer.id,
-          message: available_message,
-          state: :available
-        })
-
-      StreamsFactory.insert_consumer_message!(%{
+      ConsumersFactory.insert_consumer_event!(
         consumer_id: consumer.id,
-        message: delivered_message,
-        state: :delivered,
         not_visible_until: DateTime.add(DateTime.utc_now(), 30, :second)
-      })
+      )
 
-      conn = get(conn, ~p"/api/streams/#{stream.id}/consumers/#{consumer.id}/receive")
+      conn = get(conn, ~p"/api/http_pull_consumers/#{consumer.id}/receive")
       assert %{"data" => [message]} = json_response(conn, 200)
-      assert message["ack_id"] == cm.ack_id
-      assert message["message"]["key"] == available_message.key
+      assert message["ack_id"] == event.ack_id
     end
 
-    test "returns an available message by name", %{conn: conn, consumer: consumer, stream: stream} do
-      db_message = StreamsFactory.insert_message!(%{stream_id: stream.id})
-      StreamsFactory.insert_consumer_message!(%{consumer_id: consumer.id, message: db_message, state: :available})
+    test "returns an available message by consumer name", %{conn: conn, consumer: consumer} do
+      event = ConsumersFactory.insert_consumer_event!(consumer_id: consumer.id)
 
-      conn = get(conn, ~p"/api/streams/#{stream.name}/consumers/#{consumer.name}/receive")
+      conn = get(conn, ~p"/api/http_pull_consumers/#{consumer.name}/receive")
       assert %{"data" => [message]} = json_response(conn, 200)
-      assert message["message"]["key"] == db_message.key
+      assert message["ack_id"] == event.ack_id
     end
 
-    test "respects batch_size parameter", %{conn: conn, consumer: consumer, stream: stream} do
+    test "respects batch_size parameter", %{conn: conn, consumer: consumer} do
       for _ <- 1..3 do
-        message = StreamsFactory.insert_message!(%{stream_id: stream.id})
-        StreamsFactory.insert_consumer_message!(%{consumer_id: consumer.id, message: message, state: :available})
+        ConsumersFactory.insert_consumer_event!(consumer_id: consumer.id)
       end
 
-      conn = get(conn, ~p"/api/streams/#{stream.id}/consumers/#{consumer.id}/receive", batch_size: 1)
+      conn = get(conn, ~p"/api/http_pull_consumers/#{consumer.id}/receive", batch_size: 1)
       assert %{"data" => messages} = json_response(conn, 200)
       assert length(messages) == 1
     end
   end
 
   describe "ack" do
-    test "successfully acks a message", %{conn: conn, consumer: consumer, stream: stream} do
-      message = StreamsFactory.insert_message!(%{stream_id: stream.id})
-      cm = StreamsFactory.insert_consumer_message!(%{consumer_id: consumer.id, message: message, state: :delivered})
+    test "successfully acks a message", %{conn: conn, consumer: consumer} do
+      event = ConsumersFactory.insert_consumer_event!(consumer_id: consumer.id)
 
-      res_conn = post(conn, ~p"/api/streams/#{stream.id}/consumers/#{consumer.id}/ack", ack_ids: [cm.ack_id])
+      res_conn = post(conn, ~p"/api/http_pull_consumers/#{consumer.id}/ack", ack_ids: [event.ack_id])
       assert json_response(res_conn, 200) == %{"success" => true}
 
       # Verify the message can't be pulled again
-      conn = get(conn, ~p"/api/streams/#{stream.id}/consumers/#{consumer.id}/receive")
+      conn = get(conn, ~p"/api/http_pull_consumers/#{consumer.id}/receive")
       assert %{"data" => []} = json_response(conn, 200)
 
-      # Verify it's gone from consumer_messages
-      assert Streams.all_consumer_messages() == []
+      # Verify it's gone from consumer_events
+      assert Consumers.list_consumer_events_for_consumer(consumer.id) == []
     end
 
-    test "successfully acks a message by consumer name", %{conn: conn, consumer: consumer, stream: stream} do
-      message = StreamsFactory.insert_message!(%{stream_id: stream.id})
-      cm = StreamsFactory.insert_consumer_message!(%{consumer_id: consumer.id, message: message, state: :delivered})
+    test "successfully acks a message by consumer name", %{conn: conn, consumer: consumer} do
+      event = ConsumersFactory.insert_consumer_event!(consumer_id: consumer.id)
 
-      res_conn = post(conn, ~p"/api/streams/#{stream.name}/consumers/#{consumer.name}/ack", ack_ids: [cm.ack_id])
+      res_conn = post(conn, ~p"/api/http_pull_consumers/#{consumer.name}/ack", ack_ids: [event.ack_id])
       assert json_response(res_conn, 200) == %{"success" => true}
     end
 
-    test "allows acking a message twice", %{conn: conn, consumer: consumer, stream: stream} do
-      message = StreamsFactory.insert_message!(%{stream_id: stream.id})
-      cm = StreamsFactory.insert_consumer_message!(%{consumer_id: consumer.id, message: message, state: :delivered})
+    test "allows acking a message twice", %{conn: conn, consumer: consumer} do
+      event = ConsumersFactory.insert_consumer_event!(consumer_id: consumer.id)
 
-      res_conn = post(conn, ~p"/api/streams/#{stream.id}/consumers/#{consumer.id}/ack", ack_ids: [cm.ack_id])
+      res_conn = post(conn, ~p"/api/http_pull_consumers/#{consumer.id}/ack", ack_ids: [event.ack_id])
       assert json_response(res_conn, 200) == %{"success" => true}
 
-      conn = post(conn, ~p"/api/streams/#{stream.id}/consumers/#{consumer.id}/ack", ack_ids: [cm.ack_id])
+      conn = post(conn, ~p"/api/http_pull_consumers/#{consumer.id}/ack", ack_ids: [event.ack_id])
       assert json_response(conn, 200) == %{"success" => true}
     end
 
     test "returns 404 when acking a message belonging to another consumer", %{
       conn: conn,
-      other_consumer: other_consumer,
-      other_stream: other_stream
+      other_consumer: other_consumer
     } do
-      message = StreamsFactory.insert_message!(%{stream_id: other_stream.id})
+      event = ConsumersFactory.insert_consumer_event!(consumer_id: other_consumer.id)
 
-      cm =
-        StreamsFactory.insert_consumer_message!(%{consumer_id: other_consumer.id, message: message, state: :delivered})
-
-      conn = post(conn, ~p"/api/streams/#{other_stream.id}/consumers/#{other_consumer.id}/ack", ack_ids: [cm.ack_id])
+      conn = post(conn, ~p"/api/http_pull_consumers/#{other_consumer.id}/ack", ack_ids: [event.ack_id])
       assert json_response(conn, 404)
     end
   end
 
   describe "nack" do
-    test "successfully nacks a message", %{conn: conn, consumer: consumer, stream: stream} do
-      message = StreamsFactory.insert_message!(%{stream_id: stream.id})
-      cm = StreamsFactory.insert_consumer_message!(%{consumer_id: consumer.id, message: message, state: :delivered})
+    test "successfully nacks a message", %{conn: conn, consumer: consumer} do
+      event = ConsumersFactory.insert_consumer_event!(consumer_id: consumer.id)
 
-      res_conn = post(conn, ~p"/api/streams/#{stream.id}/consumers/#{consumer.id}/nack", ack_ids: [cm.ack_id])
+      res_conn = post(conn, ~p"/api/http_pull_consumers/#{consumer.id}/nack", ack_ids: [event.ack_id])
       assert json_response(res_conn, 200) == %{"success" => true}
-
-      # Verify it's still in consumer_messages
-      assert Streams.get_consumer_message!(consumer.id, cm.message_key).state == :available
-
       # Verify the message reappears
-      conn = get(conn, ~p"/api/streams/#{stream.id}/consumers/#{consumer.id}/receive")
+      conn = get(conn, ~p"/api/http_pull_consumers/#{consumer.id}/receive")
       assert %{"data" => [nacked_message]} = json_response(conn, 200)
-      assert nacked_message["message"]["key"] == message.key
+      assert nacked_message["ack_id"] == event.ack_id
     end
 
-    test "successfully nacks a message by consumer name", %{conn: conn, consumer: consumer, stream: stream} do
-      message = StreamsFactory.insert_message!(%{stream_id: stream.id})
-      cm = StreamsFactory.insert_consumer_message!(%{consumer_id: consumer.id, message: message, state: :delivered})
+    test "successfully nacks a message by consumer name", %{conn: conn, consumer: consumer} do
+      event = ConsumersFactory.insert_consumer_event!(consumer_id: consumer.id)
 
-      res_conn = post(conn, ~p"/api/streams/#{stream.name}/consumers/#{consumer.name}/nack", ack_ids: [cm.ack_id])
+      res_conn = post(conn, ~p"/api/http_pull_consumers/#{consumer.name}/nack", ack_ids: [event.ack_id])
       assert json_response(res_conn, 200) == %{"success" => true}
     end
 
-    test "allows nacking a message twice", %{conn: conn, consumer: consumer, stream: stream} do
-      message = StreamsFactory.insert_message!(%{stream_id: stream.id})
-      cm = StreamsFactory.insert_consumer_message!(%{consumer_id: consumer.id, message: message, state: :delivered})
+    test "allows nacking a message twice", %{conn: conn, consumer: consumer} do
+      event = ConsumersFactory.insert_consumer_event!(consumer_id: consumer.id)
 
-      res_conn = post(conn, ~p"/api/streams/#{stream.id}/consumers/#{consumer.id}/nack", ack_ids: [cm.ack_id])
+      res_conn = post(conn, ~p"/api/http_pull_consumers/#{consumer.id}/nack", ack_ids: [event.ack_id])
       assert json_response(res_conn, 200) == %{"success" => true}
 
-      conn = post(conn, ~p"/api/streams/#{stream.id}/consumers/#{consumer.id}/nack", ack_ids: [cm.ack_id])
+      conn = post(conn, ~p"/api/http_pull_consumers/#{consumer.id}/nack", ack_ids: [event.ack_id])
       assert json_response(conn, 200) == %{"success" => true}
     end
 
     test "returns 404 when nacking a message belonging to another consumer", %{
       conn: conn,
-      other_consumer: other_consumer,
-      other_stream: other_stream
+      other_consumer: other_consumer
     } do
-      message = StreamsFactory.insert_message!(%{stream_id: other_stream.id})
+      event = ConsumersFactory.insert_consumer_event!(consumer_id: other_consumer.id)
 
-      cm =
-        StreamsFactory.insert_consumer_message!(%{consumer_id: other_consumer.id, message: message, state: :delivered})
-
-      conn = post(conn, ~p"/api/streams/#{other_stream.id}/consumers/#{other_consumer.id}/nack", ack_ids: [cm.ack_id])
+      conn = post(conn, ~p"/api/http_pull_consumers/#{other_consumer.id}/nack", ack_ids: [event.ack_id])
       assert json_response(conn, 404)
     end
   end

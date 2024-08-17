1f0510f8e86958d96abb6db448a757adfeb2653a
Anthony Accomazzo <accomazz@gmail.com>
2024-08-17 13:31:51 -0700

    Add Consumers.insert_consumer_records

--- lib/sequin/consumers/query.ex
+++ lib/sequin/consumers/query.ex
@@ -3,9 +3,7 @@ defmodule Sequin.Consumers.Query do
   use Yesql, driver: Ecto, conn: Sequin.Repo
 
   # Important - include these declarations to force compilation when these files change
-  @external_resource Path.join(__DIR__, "upsert_consumer_records.sql")
   @external_resource Path.join(__DIR__, "receive_consumer_events.sql")
 
-  Yesql.defquery(Path.join(__DIR__, "upsert_consumer_records.sql"))
   Yesql.defquery(Path.join(__DIR__, "receive_consumer_events.sql"))
 end

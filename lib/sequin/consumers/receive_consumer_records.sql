e186ee01501173f907e81979ea5da62a906c34f1
Anthony Accomazzo <accomazz@gmail.com>
2024-08-17 15:43:52 -0700

    First part of ConsumerRecord.receive_for_consumer

---
+++ lib/sequin/consumers/receive_consumer_records.sql
@@ -0,0 +1,44 @@
+with deliverable_records as (
+  select
+    cr.id,
+    cr.commit_lsn,
+    cr.record_pks,
+    cr.table_oid
+  from
+    sequin_streams.consumer_records cr
+  where
+    cr.consumer_id = :consumer_id
+    and (cr.not_visible_until is null
+      or cr.not_visible_until <= :now)
+    and state != 'acked'
+    and not exists (
+      select
+        1
+      from
+        sequin_streams.consumer_records outstanding
+      where
+        outstanding.consumer_id = :consumer_id
+        and outstanding.not_visible_until > :now
+        and outstanding.record_pks = cr.record_pks
+        and outstanding.table_oid = cr.table_oid)
+    order by
+      cr.id asc
+    limit :batch_size
+    for update
+      skip locked)
+update
+  sequin_streams.consumer_records cr
+set
+  not_visible_until = :not_visible_until,
+  deliver_count = cr.deliver_count + 1,
+  last_delivered_at = :now,
+  updated_at = :now,
+  state = 'delivered'
+where
+  cr.id in (
+    select
+      dr.id
+    from
+      deliverable_records dr)
+returning
+  cr.*

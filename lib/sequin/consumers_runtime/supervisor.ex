cc4d6cc3e3f10e2b0f715d048279c21cc53a207e
thisisgoldman <emgoldma@gmail.com>
2024-08-20 11:35:51 -0700

    Fix supervission tree for consumers, support date and naive date time filters

--- lib/sequin/consumers_runtime/supervisor.ex
+++ lib/sequin/consumers_runtime/supervisor.ex
@@ -17,7 +17,11 @@ defmodule Sequin.ConsumersRuntime.Supervisor do
     Supervisor.init(children(), strategy: :one_for_one)
   end
 
-  def start_for_push_consumer(supervisor \\ ConsumersRuntime.DynamicSupervisor, consumer_or_id, opts \\ [])
+  def start_for_push_consumer(
+        supervisor \\ ConsumersRuntime.DynamicSupervisor,
+        consumer_or_id,
+        opts \\ []
+      )
 
   def start_for_push_consumer(supervisor, %HttpPushConsumer{} = consumer, opts) do
     default_opts = [consumer: consumer]
@@ -52,7 +56,7 @@ defmodule Sequin.ConsumersRuntime.Supervisor do
   defp children do
     [
       Sequin.ConsumersRuntime.Starter,
-      Sequin.DynamicSupervisor.child_spec(name: DynamicSupervisor)
+      Sequin.DynamicSupervisor.child_spec(name: ConsumersRuntime.DynamicSupervisor)
     ]
   end
 end

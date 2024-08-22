b1dc9ecdf32055d344c160e663f97a0e40ff49a3
Anthony Accomazzo <accomazz@gmail.com>
2024-08-22 14:17:21 -0700

    Tweak copy

--- assets/svelte/databases/New.svelte
+++ assets/svelte/databases/New.svelte
@@ -244,7 +244,7 @@
         disabled={validating}
         class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
       >
-        {validating ? "Validating..." : "Create Database and Replication Slot"}
+        {validating ? "Validating..." : "Create Database"}
       </button>
     </div>
   </form>

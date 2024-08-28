81075a508ac23e23344f7532a1be834fd2f46096
Anthony Accomazzo <accomazz@gmail.com>
2024-08-28 10:28:31 -0700

    Revert accounts.is_temp

--- lib/sequin/accounts/account.ex
+++ lib/sequin/accounts/account.ex
@@ -3,31 +3,19 @@ defmodule Sequin.Accounts.Account do
   use Sequin.ConfigSchema
 
   import Ecto.Changeset
-  import Ecto.Query, only: [from: 2]
+  # import Ecto.Query, only: [from: 2]
 
   alias Sequin.Accounts.Account
 
   typed_schema "accounts" do
-    field :is_temp, :boolean, null: false, default: true
-
     timestamps()
   end
 
   def changeset(%Account{} = account, attrs) do
-    account
-    |> cast(attrs, [:is_temp])
-    |> validate_required([:is_temp])
+    cast(account, attrs, [])
   end
 
-  def where_temp(query \\ base_query()) do
-    from([account: a] in query, where: a.is_temp == true)
-  end
-
-  def where_inserted_before(query \\ base_query(), cutoff_time) do
-    from([account: a] in query, where: a.inserted_at < ^cutoff_time)
-  end
-
-  defp base_query(query \\ __MODULE__) do
-    from(a in query, as: :account)
-  end
+  # defp base_query(query \\ __MODULE__) do
+  #   from(a in query, as: :account)
+  # end
 end

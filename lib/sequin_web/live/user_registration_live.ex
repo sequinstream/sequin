fcbbd399f4243071033b62c207935b9587aec945
Anthony Accomazzo <accomazz@gmail.com>
2024-08-29 15:17:07 -0700

    Add 'Continue with GitHub' button to registration page

--- lib/sequin_web/live/user_registration_live.ex
+++ lib/sequin_web/live/user_registration_live.ex
@@ -19,24 +19,57 @@ defmodule SequinWeb.UserRegistrationLive do
           </:subtitle>
         </.header>
 
-        <.simple_form
-          for={@form}
-          id="registration_form"
-          phx-submit="save"
-          phx-change="validate"
-          phx-trigger-action={@trigger_submit}
-          action={~p"/login?_action=registered"}
-          method="post"
-        >
-          <.input field={@form[:email]} type="email" label="Email" required />
-          <.input field={@form[:password]} type="password" label="Password" required />
-
-          <:actions>
-            <.button phx-disable-with="Creating account..." class="w-full">
-              Create an account <span aria-hidden="true">→</span>
+        <div class="mt-6 space-y-4">
+          <.link href="/auth/github">
+            <.button
+              class="w-full flex items-center justify-center gap-2 mb-10 disabled:opacity-50 disabled:cursor-not-allowed"
+              phx-click="github_register"
+              id="github-register-button"
+              disabled={@github_loading}
+            >
+              <svg class="w-5 h-5" viewBox="0 0 24 24" fill="currentColor">
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.164 6.839 9.49.5.092.682-.217.682-.482 0-.237-.009-.866-.013-1.7-2.782.603-3.369-1.34-3.369-1.34-.454-1.156-1.11-1.463-1.11-1.463-.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.087 2.91.831.092-.646.35-1.086.636-1.336-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.03-2.682-.103-.253-.447-1.27.098-2.646 0 0 .84-.269 2.75 1.025A9.564 9.564 0 0112 6.844c.85.004 1.705.114 2.504.336 1.909-1.294 2.747-1.025 2.747-1.025.546 1.376.202 2.393.1 2.646.64.698 1.026 1.591 1.026 2.682 0 3.841-2.337 4.687-4.565 4.935.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.743 0 .267.18.578.688.48C19.138 20.161 22 16.416 22 12c0-5.523-4.477-10-10-10z"
+                />
+              </svg>
+              <%= if @github_loading do %>
+                Redirecting to GitHub...
+              <% else %>
+                Continue with GitHub
+              <% end %>
             </.button>
-          </:actions>
-        </.simple_form>
+          </.link>
+
+          <div class="relative">
+            <div class="absolute inset-0 flex items-center">
+              <div class="w-full border-t border-gray-300"></div>
+            </div>
+            <div class="relative flex justify-center text-sm">
+              <span class="px-2 bg-white text-gray-500">or</span>
+            </div>
+          </div>
+
+          <.simple_form
+            for={@form}
+            id="registration_form"
+            phx-submit="save"
+            phx-change="validate"
+            phx-trigger-action={@trigger_submit}
+            action={~p"/login?_action=registered"}
+            method="post"
+          >
+            <.input field={@form[:email]} type="email" label="Email" required />
+            <.input field={@form[:password]} type="password" label="Password" required />
+
+            <:actions>
+              <.button phx-disable-with="Creating account..." class="w-full">
+                Create an account <span aria-hidden="true">→</span>
+              </.button>
+            </:actions>
+          </.simple_form>
+        </div>
       </div>
     </div>
     """
@@ -48,6 +81,7 @@ defmodule SequinWeb.UserRegistrationLive do
     socket =
       socket
       |> assign(trigger_submit: false)
+      |> assign(github_loading: false)
       |> assign_form(changeset)
 
     {:ok, socket, temporary_assigns: [form: nil]}
@@ -73,6 +107,10 @@ defmodule SequinWeb.UserRegistrationLive do
   def handle_event("validate", %{"user" => user_params}, socket) do
     changeset = Accounts.change_user_registration(%User{}, user_params)
     {:noreply, assign_form(socket, changeset)}
+  end
+
+  def handle_event("github_register", _params, socket) do
+    {:noreply, assign(socket, github_loading: true)}
   end
 
   defp assign_form(socket, %Ecto.Changeset{} = changeset) do

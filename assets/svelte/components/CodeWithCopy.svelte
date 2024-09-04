f1ddec2b7c680c021b93bca95b78f5235b27d228
RTLS <carter.e.pedersen@gmail.com>
2024-09-04 15:16:17 -0700

    💄 Database form improvements from Demo #2

--- assets/svelte/components/CodeWithCopy.svelte
+++ assets/svelte/components/CodeWithCopy.svelte
@@ -2,14 +2,14 @@
   import CopyIcon from "./CopyIcon.svelte";
 
   export let language: string = "";
-  let codeElement: HTMLElement;
-  $: codeContent = codeElement?.textContent || "";
+  export let code: string = "";
+
+  $: codeContent = code.trim();
 </script>
 
 <div class="relative bg-muted p-4 rounded-md mb-4">
-  <pre class="text-sm overflow-x-auto"><code
-      bind:this={codeElement}
-      class={language}><slot /></code
+  <pre class="text-sm overflow-x-auto"><code class={language}
+      >{codeContent}</code
     ></pre>
   <div class="absolute top-2 right-2">
     <CopyIcon content={codeContent} />

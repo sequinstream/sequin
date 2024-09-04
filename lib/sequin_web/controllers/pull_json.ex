23aafdc0e240cdf9354b658bb40e6b36b6880aa6
RTLS <carter.e.pedersen@gmail.com>
2024-09-04 12:10:12 -0700

    ✨ PullJSON for records

--- lib/sequin_web/controllers/pull_json.ex
+++ lib/sequin_web/controllers/pull_json.ex
@@ -1,6 +1,8 @@
 defmodule SequinWeb.PullJSON do
   alias Sequin.Consumers.ConsumerEvent
   alias Sequin.Consumers.ConsumerEventData
+  alias Sequin.Consumers.ConsumerRecord
+  alias Sequin.Consumers.ConsumerRecordData
 
   def render("receive.json", %{messages: messages}) do
     %{data: Enum.map(messages, &render_message/1)}
@@ -13,11 +15,24 @@ defmodule SequinWeb.PullJSON do
     }
   end
 
+  defp render_message(%ConsumerRecord{} = record) do
+    %{
+      ack_id: record.ack_id,
+      data: render_data(record.data)
+    }
+  end
+
   defp render_data(%ConsumerEventData{action: action, record: record, changes: changes}) do
     %{
       action: action,
       record: record,
       changes: changes
+    }
+  end
+
+  defp render_data(%ConsumerRecordData{record: record}) do
+    %{
+      record: record
     }
   end
 end

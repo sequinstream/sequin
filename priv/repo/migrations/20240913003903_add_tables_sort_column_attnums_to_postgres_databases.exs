da1b1ab127b2203ae5f9e53601d906077cb7faac
Anthony Accomazzo <accomazz@gmail.com>
2024-09-13 14:52:36 -0700

    [row consumer] Implement RecordHandler

--- priv/repo/migrations/20240913003903_add_tables_sort_column_attnums_to_postgres_databases.exs
+++ priv/repo/migrations/20240913003903_add_tables_sort_column_attnums_to_postgres_databases.exs
@@ -4,16 +4,21 @@ defmodule Sequin.Repo.Migrations.AddTablesSortColumnAttnumsToPostgresDatabases d
   @config_schema_prefix Application.compile_env!(:sequin, Sequin.Repo)
                         |> Keyword.fetch!(:config_schema_prefix)
 
-  def change do
+  def up do
     alter table(:postgres_databases) do
       add :tables_sort_column_attnums, :map
     end
 
-    execute "update #{@config_schema_prefix}.postgres_databases set tables_sort_column_attnums = '{}'::jsonb",
-            "select 1"
+    execute "update #{@config_schema_prefix}.postgres_databases set tables_sort_column_attnums = '{}'::jsonb"
 
     alter table(:postgres_databases) do
       modify :tables_sort_column_attnums, :map, null: false
+    end
+  end
+
+  def down do
+    alter table(:postgres_databases) do
+      remove :tables_sort_column_attnums
     end
   end
 end

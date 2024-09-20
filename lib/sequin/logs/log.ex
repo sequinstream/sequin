d76da1f77958f14a679f8e6716ab105612cd7326
RTLS <carter.e.pedersen@gmail.com>
2024-09-20 16:58:09 -0700

    ✨ Sequin.Logs for consumer messages

---
+++ lib/sequin/logs/log.ex
@@ -0,0 +1,29 @@
+defmodule Sequin.Logs.Log do
+  @moduledoc false
+  alias __MODULE__
+
+  defstruct [
+    :account_id,
+    :timestamp,
+    :message,
+    :status
+  ]
+
+  def from_datadog_log(log) do
+    attributes = get_in(log, ["attributes", "attributes"])
+
+    %Log{
+      account_id: get_in(attributes, ["account_id"]),
+      timestamp:
+        attributes
+        |> get_in(["syslog", "timestamp"])
+        |> DateTime.from_iso8601()
+        |> case do
+          {:ok, timestamp, 0} -> timestamp
+          {:error, _reason} -> nil
+        end,
+      message: get_in(log, ["attributes", "message"]),
+      status: get_in(log, ["attributes", "status"])
+    }
+  end
+end

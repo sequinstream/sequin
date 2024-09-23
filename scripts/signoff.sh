d9e54b63330076df88691a35553220cdcb55a0bd
Anthony Accomazzo <accomazz@gmail.com>
2024-09-23 15:57:22 -0700

    Add cli validation to signoff task

--- scripts/signoff.sh
+++ scripts/signoff.sh
@@ -76,6 +76,15 @@ run_step "mix format --check-formatted"
 run_step "MIX_ENV=prod mix compile --warnings-as-errors"
 run_step "mix test"
 
+# Run CLI tests
+cd cli
+run_step "go test ./cli"
+run_step "go fmt ./..."
+run_step "go vet ./..."
+run_step "go build -o /dev/null ./..."
+# Return to the project root
+cd ..
+
 # Run cspell check
 run_step "make spellcheck"
 

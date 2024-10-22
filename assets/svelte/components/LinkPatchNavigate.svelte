96e40f30c57ff9d6de35c8d197f7b2e3296f6acf
Anthony Accomazzo <accomazz@gmail.com>
2024-10-22 11:34:42 -0700

    Add LinkPatchNavigate

---
+++ assets/svelte/components/LinkPatchNavigate.svelte
@@ -0,0 +1,10 @@
+<!-- https://github.com/woutdp/live_svelte?tab=readme-ov-file#liveview-live-navigation-events -->
+<script>
+  export let href;
+</script>
+
+<a {href} {...$$restProps} data-phx-link="patch" data-phx-link-state="push">
+  {#if $$slots.default}
+    <slot />
+  {/if}
+</a>

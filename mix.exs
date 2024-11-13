d8587311012960ba90e705f55156db33ec431690
RTLS <carter.e.pedersen@gmail.com>
2024-11-12 18:52:54 -0800

    🗑️ Remove dev.ex

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Sequin.MixProject do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(:dev), do: ["lib", "test/support/dev"]
+  defp elixirc_paths(:dev), do: ["lib"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.

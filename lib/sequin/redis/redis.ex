1e872a20015162125ba3f947f992d51b3cfbd68b
RTLS <carter.e.pedersen@gmail.com>
2024-11-14 14:14:06 -0800

    ✨ Redis Test Connect

--- lib/sequin/redis/redis.ex
+++ lib/sequin/redis/redis.ex
@@ -9,6 +9,7 @@ defmodule Sequin.Redis do
               :ok | {:error, Error.t()}
   @callback message_count(RedisDestination.t()) :: {:ok, non_neg_integer()} | {:error, Error.t()}
   @callback client_info(RedisDestination.t()) :: {:ok, String.t()} | {:error, Error.t()}
+  @callback test_connection(RedisDestination.t()) :: :ok | {:error, Error.t()}
 
   @spec send_messages(RedisDestination.t(), [any()]) :: :ok | {:error, Error.t()}
   def send_messages(%RedisDestination{} = destination, messages) do
@@ -23,6 +24,11 @@ defmodule Sequin.Redis do
   @spec client_info(RedisDestination.t()) :: {:ok, String.t()} | {:error, Error.t()}
   def client_info(%RedisDestination{} = destination) do
     impl().client_info(destination)
+  end
+
+  @spec test_connection(RedisDestination.t()) :: :ok | {:error, Error.t()}
+  def test_connection(%RedisDestination{} = destination) do
+    impl().test_connection(destination)
   end
 
   defp impl do

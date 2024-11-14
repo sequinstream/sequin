a2e39f86140b3d7eef86a239e9dd39933b6b6cbd
RTLS <carter.e.pedersen@gmail.com>
2024-11-14 13:29:45 -0800

    ✨ Redis Client and ConnectionCache

--- lib/sequin/redis/redis.ex
+++ lib/sequin/redis/redis.ex
@@ -4,10 +4,16 @@ defmodule Sequin.Redis do
   alias Sequin.Error
 
   @callback send_messages(%RedisDestination{}, [any()]) :: :ok | {:error, Error.t()}
+  @callback message_count(%RedisDestination{}) :: non_neg_integer()
 
   @spec send_messages(%RedisDestination{}, [any()]) :: :ok | {:error, Error.t()}
   def send_messages(%RedisDestination{} = destination, messages) do
     impl().send_messages(destination, messages)
+  end
+
+  @spec message_count(%RedisDestination{}) :: non_neg_integer()
+  def message_count(%RedisDestination{} = destination) do
+    impl().message_count(destination)
   end
 
   defp impl do

c75870e18525396bbcc0b31b875b920e1ce8b9a7
Anthony Accomazzo <accomazz@gmail.com>
2024-11-24 19:17:59 -0800

    :sparkles: GCP Pub/Sub runtime and polish

--- lib/sequin/consumers/gcp_pubsub_sink.ex
+++ lib/sequin/consumers/gcp_pubsub_sink.ex
@@ -5,7 +5,8 @@ defmodule Sequin.Consumers.GcpPubsubSink do
 
   import Ecto.Changeset
 
-  alias Sequin.Encrypted
+  alias Sequin.Gcp.Credentials
+  alias Sequin.Gcp.PubSub
 
   @derive {Jason.Encoder, only: [:project_id, :topic_id]}
   @primary_key false
@@ -13,18 +14,20 @@ defmodule Sequin.Consumers.GcpPubsubSink do
     field :type, Ecto.Enum, values: [:gcp_pubsub], default: :gcp_pubsub
     field :project_id, :string
     field :topic_id, :string
-    field :credentials, Encrypted.Field
     field :connection_id, :string
+
+    embeds_one :credentials, Credentials
   end
 
   def changeset(struct, params) do
     struct
     |> cast(params, [
       :project_id,
-      :topic_id,
-      :credentials
+      :topic_id
     ])
-    |> validate_required([:project_id, :topic_id, :credentials])
+    |> validate_required([:project_id, :topic_id])
+    |> validate_credential_param(params)
+    |> cast_embed(:credentials, required: true)
     |> validate_length(:project_id, max: 255)
     |> validate_length(:topic_id, max: 255)
     |> validate_format(:project_id, ~r/^[a-z][-a-z0-9]{4,28}[a-z0-9]$/,
@@ -34,21 +37,21 @@ defmodule Sequin.Consumers.GcpPubsubSink do
     |> validate_format(:topic_id, ~r/^[a-zA-Z][a-zA-Z0-9-_.~+%]{2,254}$/,
       message: "must be between 3 and 255 characters and match the pattern: [a-zA-Z][a-zA-Z0-9-_.~+%]*"
     )
-    |> validate_credentials()
     |> put_new_connection_id()
   end
 
-  defp validate_credentials(changeset) do
-    case get_field(changeset, :credentials) do
-      nil ->
-        changeset
+  @doc """
+  Creates a new PubSub client for the given sink configuration.
+  """
+  def pubsub_client(%__MODULE__{} = sink) do
+    PubSub.new(
+      sink.project_id,
+      sink.credentials
+    )
+  end
 
-      credentials ->
-        case Jason.decode(credentials) do
-          {:ok, _} -> changeset
-          {:error, _} -> add_error(changeset, :credentials, "must be valid JSON service account credentials")
-        end
-    end
+  def topic_path(%__MODULE__{} = sink) do
+    "projects/#{sink.project_id}/topics/#{sink.topic_id}"
   end
 
   defp put_new_connection_id(changeset) do
@@ -58,7 +61,12 @@ defmodule Sequin.Consumers.GcpPubsubSink do
     end
   end
 
-  def topic_path(%__MODULE__{} = sink) do
-    "projects/#{sink.project_id}/topics/#{sink.topic_id}"
+  defp validate_credential_param(changeset, %{"credentials" => creds}) when is_binary(creds) do
+    case Jason.decode(creds) do
+      {:ok, _} -> changeset
+      {:error, _} -> add_error(changeset, :credentials, "must be valid JSON service account credentials")
+    end
   end
+
+  defp validate_credential_param(changeset, _params), do: changeset
 end
---
+++ lib/sequin/gcp/credentials.ex
@@ -0,0 +1,87 @@
+defmodule Sequin.Gcp.Credentials do
+  @moduledoc """
+  Schema for Google Cloud Platform credentials.
+  Supports different authentication methods including service accounts,
+  OAuth client credentials, API keys, etc.
+  """
+  use Ecto.Schema
+  use TypedEctoSchema
+
+  import Ecto.Changeset
+
+  alias Sequin.Encrypted
+
+  @derive {Inspect, except: [:private_key]}
+  @primary_key false
+  typed_embedded_schema do
+    field :type, :string
+    field :project_id, :string
+
+    # Service Account fields
+    field :private_key_id, :string
+    field :private_key, Encrypted.Field
+    field :client_email, :string
+    field :client_id, :string
+    field :auth_uri, :string
+    field :token_uri, :string
+    field :auth_provider_x509_cert_url, :string
+    field :client_x509_cert_url, :string
+    field :universe_domain, :string
+
+    # OAuth Client fields
+    field :client_secret, Encrypted.Field
+
+    # API Key field
+    field :api_key, Encrypted.Field
+  end
+
+  def changeset(struct, params) do
+    struct
+    |> cast(params, [
+      :type,
+      :project_id,
+      # Service Account fields
+      :private_key_id,
+      :private_key,
+      :client_email,
+      :client_id,
+      :auth_uri,
+      :token_uri,
+      :auth_provider_x509_cert_url,
+      :client_x509_cert_url,
+      :universe_domain,
+      # OAuth fields
+      :client_secret,
+      # API Key
+      :api_key
+    ])
+    |> validate_required([:type, :project_id])
+    |> validate_type_specific_fields()
+  end
+
+  defp validate_type_specific_fields(changeset) do
+    case get_field(changeset, :type) do
+      "service_account" ->
+        changeset
+        |> validate_required([
+          :private_key_id,
+          :private_key,
+          :client_email,
+          :client_id
+        ])
+        |> validate_format(:client_email, ~r/@.*\.iam\.gserviceaccount\.com$/)
+
+      "oauth_client" ->
+        validate_required(changeset, [:client_id, :client_secret])
+
+      "api_key" ->
+        validate_required(changeset, [:api_key])
+
+      type when type in [nil, ""] ->
+        add_error(changeset, :type, "is required")
+
+      type ->
+        add_error(changeset, :type, "#{type} is not a supported credential type")
+    end
+  end
+end

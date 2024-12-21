133e0249c1d6e1829211fda912534556ccc31836
Anthony Accomazzo <accomazz@gmail.com>
2024-12-20 16:51:30 -0800

    ♻️ Move from Redix to eredis (better cluster support)

--- lib/sequin/redis.ex
+++ lib/sequin/redis.ex
@@ -1,30 +1,81 @@
 defmodule Sequin.Redis do
   @moduledoc false
+  alias Sequin.Error
+  alias Sequin.Error.ServiceError
+
+  require Logger
+
+  @type command :: [any()]
+  @type return_value :: nil | binary() | [binary() | nonempty_list()]
+  @type pipeline_return_value :: nil | binary() | [binary() | nonempty_list()] | ServiceError.t()
+
   @config Application.compile_env(:sequin, __MODULE__, [])
 
   def child_spec do
-    url = Keyword.fetch!(@config, :url)
+    %{host: host, port: port} = URI.parse(Keyword.fetch!(@config, :url))
     opts = Keyword.get(@config, :opts, [])
-    opts = [name: __MODULE__] ++ opts
-    {Redix, {url, opts}}
+    opts = [name: {:local, __MODULE__}, host: to_charlist(host), port: port] ++ opts
+
+    %{
+      id: __MODULE__,
+      type: :worker,
+      restart: :permanent,
+      start: {:eredis, :start_link, [opts]}
+    }
   end
 
-  @spec command(Redix.command(), keyword()) ::
-          {:ok, Redix.Protocol.redis_value()}
-          | {:error, atom() | Redix.Error.t() | Redix.ConnectionError.t()}
-  def command(command, opts \\ []) do
-    Redix.command(__MODULE__, command, opts)
+  @spec command(command()) :: {:ok, return_value()} | {:error, ServiceError.t()}
+  def command(command) do
+    res =
+      __MODULE__
+      |> :eredis.q(command)
+      |> parse_result()
+
+    case res do
+      {:ok, result} ->
+        {:ok, result}
+
+      {:error, :no_connection} ->
+        {:error, Error.service(service: :redis, code: :no_connection, message: "No connection to Redis")}
+
+      {:error, error} when is_binary(error) ->
+        Logger.error("Redis command failed: #{error}", error: error)
+        {:error, Error.service(service: :redis, code: :command_failed, message: error)}
+    end
   end
 
-  @spec command!(Redix.command(), keyword()) :: Redix.Protocol.redis_value()
-  def command!(command, opts \\ []) do
-    Redix.command!(__MODULE__, command, opts)
+  @spec command!(command()) :: return_value()
+  def command!(command) do
+    res = __MODULE__ |> :eredis.q(command) |> parse_result()
+
+    case res do
+      {:ok, result} -> result
+      {:error, error} -> raise error
+    end
   end
 
-  @spec pipeline([Redix.command()], keyword()) ::
-          {:ok, [Redix.Protocol.redis_value()]}
-          | {:error, atom() | Redix.Error.t() | Redix.ConnectionError.t()}
-  def pipeline(commands, opts \\ []) do
-    Redix.pipeline(__MODULE__, commands, opts)
+  @spec pipeline([command()]) :: {:ok, [pipeline_return_value()]} | {:error, ServiceError.t()}
+  def pipeline(commands) do
+    case :eredis.qp(__MODULE__, commands) do
+      results when is_list(results) ->
+        # Convert eredis results to Redix-style results
+        {:ok,
+         Enum.map(results, fn
+           {:ok, :undefined} ->
+             nil
+
+           {:ok, value} ->
+             value
+
+           {:error, error} when is_binary(error) ->
+             Error.service(service: :redis, code: :command_failed, message: error)
+         end)}
+
+      {:error, :no_connection} ->
+        {:error, Error.service(service: :redis, code: :no_connection, message: "No connection to Redis")}
+    end
   end
+
+  defp parse_result({:ok, :undefined}), do: {:ok, nil}
+  defp parse_result(result), do: result
 end

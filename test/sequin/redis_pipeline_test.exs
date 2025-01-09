c78f7ccae07f98988534d2ed02e2f177d9df5c78
RTLS <carter.e.pedersen@gmail.com>
2025-01-09 10:38:03 -0800

    ♻️  Refactor :skip_load_from_postgres to :persisted_mode

--- test/sequin/redis_pipeline_test.exs
+++ test/sequin/redis_pipeline_test.exs
@@ -159,7 +159,7 @@ defmodule Sequin.ConsumersRuntime.RedisPipelineTest do
 
       consumer_record = ConsumersFactory.deliverable_consumer_record(consumer_id: consumer.id)
 
-      start_supervised!({SlotMessageStore, [consumer: consumer, test_pid: test_pid, skip_load_from_postgres?: true]})
+      start_supervised!({SlotMessageStore, [consumer: consumer, test_pid: test_pid, persisted_mode?: false]})
       SlotMessageStore.put_messages(consumer.id, [consumer_record])
 
       start_supervised!({RedisPipeline, [consumer: consumer, test_pid: test_pid]})

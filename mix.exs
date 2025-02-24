1badf3982e4f56000776c031ee995d4cb161022f
RTLS <carter.e.pedersen@gmail.com>
2025-02-24 14:51:24 -0800

    🔨 mix assets.setup

--- mix.exs
+++ mix.exs
@@ -111,13 +111,7 @@ defmodule Sequin.MixProject do
         &remove_consumer_messages_log/1
       ],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
-      "assets.build": ["tailwind sequin", "esbuild sequin"],
-      "assets.deploy": [
-        "tailwind sequin --minify",
-        "cmd --cd assets node build.js --deploy",
-        "phx.digest"
-      ]
+      "assets.setup": ["cmd --cd assets npm install"]
     ]
   end
 

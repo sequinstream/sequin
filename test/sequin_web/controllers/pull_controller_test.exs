b940718bbdd028e0bfc41b240594b3c76df5e76f
Anthony Accomazzo <accomazz@gmail.com>
2025-02-27 10:42:58 -0800

    Increase timeout for flaky test

--- test/sequin_web/controllers/pull_controller_test.exs
+++ test/sequin_web/controllers/pull_controller_test.exs
@@ -139,7 +139,7 @@ defmodule SequinWeb.PullControllerTest do
         SlotMessageStore.put_messages(consumer.id, [record])
       end
 
-      assert_elapsed_under(100, fn ->
+      assert_elapsed_under(500, fn ->
         conn = get(conn, ~p"/api/sequin_streams/#{consumer.id}/receive", max_batch_size: 3, wait_for: 5000)
         assert %{"data" => messages} = json_response(conn, 200)
         assert length(messages) == 3

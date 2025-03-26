af14b0d3bda7a15ce903480c6a20d470239345d7
RTLS <carter.e.pedersen@gmail.com>
2025-03-26 15:33:44 -0700

    🗂️ Add :transform enum to sink_consumers

---
+++ priv/repo/migrations/20250326222440_add_transform_to_sink_consumers.exs
@@ -0,0 +1,14 @@
+defmodule Sequin.Repo.Migrations.AddTransformToSinkConsumers do
+  use Ecto.Migration
+
+  @config_schema Application.compile_env(:sequin, [Sequin.Repo, :config_schema_prefix])
+
+  def change do
+    execute "create type #{@config_schema}.transform_type as enum ('none', 'record_only');",
+            "drop type #{@config_schema}.transform_type"
+
+    alter table(:sink_consumers, prefix: @config_schema) do
+      add :transform, :"#{@config_schema}.transform_type", default: "none"
+    end
+  end
+end

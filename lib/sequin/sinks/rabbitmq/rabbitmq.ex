0487de9eb1887062d20acf74a2c1b6cabcfb604b
RTLS <carter.e.pedersen@gmail.com>
2025-03-27 16:49:29 -0700

    ✨ Add transforms to sequin.yaml

--- lib/sequin/sinks/rabbitmq/rabbitmq.ex
+++ lib/sequin/sinks/rabbitmq/rabbitmq.ex
@@ -3,14 +3,15 @@ defmodule Sequin.Sinks.RabbitMq do
   alias Sequin.Consumers.ConsumerEvent
   alias Sequin.Consumers.ConsumerRecord
   alias Sequin.Consumers.RabbitMqSink
+  alias Sequin.Consumers.SinkConsumer
   alias Sequin.Error
 
-  @callback send_messages(RabbitMqSink.t(), [ConsumerRecord.t() | ConsumerEvent.t()]) ::
+  @callback send_messages(SinkConsumer.t(), [ConsumerRecord.t() | ConsumerEvent.t()]) ::
               :ok | {:error, Error.t()}
   @callback test_connection(RabbitMqSink.t()) :: :ok | {:error, Error.t()}
 
   @client Application.compile_env(:sequin, :rabbitmq_module, Sequin.Sinks.RabbitMq.Client)
-  defdelegate send_messages(sink, messages), to: @client
+  defdelegate send_messages(consumer, messages), to: @client
 
   defdelegate test_connection(sink), to: @client
 end

8fdd89bd3a576b63900672429279a16de8ec9169
RTLS <carter.e.pedersen@gmail.com>
2025-04-02 12:15:08 -0700

    🗂️ Migrating legacy_transform to transforms

--- priv/repo/migrations/20250401181637_drop_sequence_id_from_transforms.exs
+++ priv/repo/migrations/20250401181637_drop_sequence_id_from_transforms.exs
@@ -3,9 +3,15 @@ defmodule Sequin.Repo.Migrations.DropSequenceIdFromTransforms do
 
   @config_schema Application.compile_env(:sequin, [Sequin.Repo, :config_schema_prefix])
 
-  def change do
+  def up do
     alter table(:transforms, prefix: @config_schema) do
       remove :sequence_id
+    end
+  end
+
+  def down do
+    alter table(:transforms, prefix: @config_schema) do
+      add :sequence_id, references(:sequences, on_delete: :delete_all, prefix: @config_schema)
     end
   end
 end

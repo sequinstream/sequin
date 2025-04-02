d9738e07e1cb137a1cd7f347ca452c359bf0a1b2
Anthony Accomazzo <accomazz@gmail.com>
2025-04-01 20:07:10 -0700

    🗑️ Remove outdated example project

--- examples/js_orders_fanout/src/sequin.js
+++
@@ -1,9 +0,0 @@
-import { Client } from "@sequinstream/sequin-js";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const baseUrl = process.env.SEQUIN_URL || "http://localhost:7376";
-const sequin = Client.init({ baseUrl });
-
-export { sequin, baseUrl };

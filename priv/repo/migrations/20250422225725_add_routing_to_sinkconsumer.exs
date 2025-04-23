875e8b48274e9b4ebdfd6b64ee6a277e1086de0f
Russell McQueeney <rmcq@utexas.edu>
2025-04-23 10:11:54 -0700

    migration

---
+++ priv/repo/migrations/20250422225725_add_routing_to_sinkconsumer.exs
@@ -0,0 +1,14 @@
+defmodule Sequin.Repo.Migrations.AddRoutingToSinkconsumer do
+  use Ecto.Migration
+
+  @config_schema Application.compile_env(:sequin, [Sequin.Repo, :config_schema_prefix])
+
+  def change do
+    execute """
+    alter table #{@config_schema}.sink_consumers add column routing_id uuid references #{@config_schema}.transforms(id)
+    """,
+    """
+    alter table #{@config_schema}.sink_consumers drop column routing_id
+    """
+  end
+end

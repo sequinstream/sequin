a4053d9866a32a463eae17d372eaaf1a867212df
Russell McQueeney <rmcq@utexas.edu>
2025-04-24 16:35:51 -0700

    can't do that with check constraint

--- priv/repo/migrations/20250422225725_add_routing_to_sinkconsumer.exs
+++ priv/repo/migrations/20250422225725_add_routing_to_sinkconsumer.exs
@@ -12,21 +12,32 @@ defmodule Sequin.Repo.Migrations.AddRoutingToSinkconsumer do
             """
 
     execute """
-            alter table #{@config_schema}.sink_consumers
-            add constraint routing_id_points_to_routing_transform
-            check (
-              routing_id is null or
-              exists (
-                select 1
-                from #{@config_schema}.transforms
-                where transforms.id = sink_consumers.routing_id
-                  and transforms.type = 'routing'
-              )
-            )
+            create or replace function #{@config_schema}.check_routing_id_type()
+            returns trigger as $$
+            begin
+              if new.routing_id is not null then
+                if not exists (
+                  select 1 from #{@config_schema}.transforms 
+                  where id = new.routing_id and type = 'routing'
+                ) then
+                  raise exception 'routing_id must reference a transform with type ''routing''';
+                end if;
+              end if;
+              return new;
+            end;
+            $$ language plpgsql;
             """,
             """
-            alter table #{@config_schema}.sink_consumers
-            drop constraint routing_id_points_to_routing_transform
+            drop function if exists #{@config_schema}.check_routing_id_type() cascade;
+            """
+
+    execute """
+            create trigger enforce_routing_id_transform_type
+            before insert or update on #{@config_schema}.sink_consumers
+            for each row execute function #{@config_schema}.check_routing_id_type();
+            """,
+            """
+            drop trigger if exists enforce_routing_id_transform_type on #{@config_schema}.sink_consumers;
             """
   end
 end

979810b8dcdeca1ad71777ed74f3295d127d1cc2
Russell McQueeney <rmcq@utexas.edu>
2025-04-24 16:11:55 -0700

    check constraint

--- priv/repo/migrations/20250422225725_add_routing_to_sinkconsumer.exs
+++ priv/repo/migrations/20250422225725_add_routing_to_sinkconsumer.exs
@@ -10,5 +10,23 @@ defmodule Sequin.Repo.Migrations.AddRoutingToSinkconsumer do
             """
             alter table #{@config_schema}.sink_consumers drop column routing_id
             """
+
+    execute """
+            alter table #{@config_schema}.sink_consumers
+            add constraint routing_id_points_to_routing_transform
+            check (
+              routing_id is null or
+              exists (
+                select 1
+                from #{@config_schema}.transforms
+                where transforms.id = sink_consumers.routing_id
+                  and transforms.type = 'routing'
+              )
+            )
+            """,
+            """
+            alter table #{@config_schema}.sink_consumers
+            drop constraint routing_id_points_to_routing_transform
+            """
   end
 end

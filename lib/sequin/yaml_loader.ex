ab268d35b6b62b5fefe6daab01c04cdf09bc7f61
Russell McQueeney <rmcq@utexas.edu>
2025-05-07 13:55:35 -0700

    Clarify error message when creating replication slot

--- lib/sequin/yaml_loader.ex
+++ lib/sequin/yaml_loader.ex
@@ -425,13 +425,13 @@ defmodule Sequin.YamlLoader do
           {:error, error} when is_exception(error) ->
             {:error,
              Error.bad_request(
-               message: "Error creating replication slot for database '#{db.name}': #{Exception.message(error)}"
+               message: "Error creating Sequin replication slot for database '#{db.name}': #{Exception.message(error)}"
              )}
 
           {:error, %Ecto.Changeset{} = changeset} ->
             {:error,
              Error.bad_request(
-               message: "Error creating replication slot for database '#{db.name}': #{inspect(changeset, pretty: true)}"
+               message: "Error creating Sequin replication slot for database '#{db.name}': #{inspect(changeset, pretty: true)}"
              )}
         end
 

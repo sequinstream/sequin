115907b151884c2e982a7eb26e25773638835783
Russell McQueeney <rmcq@utexas.edu>
2025-05-07 19:32:04 -0700

    .

--- lib/sequin_web/metrics_endpoint.ex
+++ lib/sequin_web/metrics_endpoint.ex
@@ -1,5 +1,13 @@
 defmodule SequinWeb.MetricsEndpoint do
   use Phoenix.Endpoint, otp_app: :sequin
 
+  plug :metrics_auth
   plug Sequin.PrometheusExporter
+
+  defp metrics_auth(conn, _opts) do
+    case Application.get_env(:sequin, :metrics_basic_auth) do
+      nil -> conn
+      conf -> Plug.BasicAuth.basic_auth(conn, conf)
+    end
+  end
 end

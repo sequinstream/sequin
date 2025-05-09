920862f5bdeac7d4f48c909bf59e9b6db5090f0c
Davo <davoclavo@users.noreply.github.com>
2025-05-09 13:30:00 -0600

    feat: show clearer connectivity errors and run extra checks for IPv6 (#1374)

--- lib/sequin/network_utils.ex
+++ lib/sequin/network_utils.ex
@@ -20,7 +20,7 @@ defmodule Sequin.NetworkUtils do
   end
 
   @doc """
-  Pings a host on a port to see if it is reachable.
+  Establishes a test TCP connection to a host on a port to see if it is reachable.
   """
   @spec test_tcp_reachability(host :: String.t(), port :: number(), ipv6 :: boolean(), timeout :: number()) ::
           :ok | {:error, Error.t()}
@@ -28,9 +28,43 @@ defmodule Sequin.NetworkUtils do
     with :ok <- validate_port(port) do
       case :gen_tcp.connect(to_charlist(host), port, ipv6_opts(ipv6), timeout) do
         {:ok, port} when is_port(port) ->
-          # Succcess, we could reach host
+          # Success, we could reach host
           :gen_tcp.close(port)
           :ok
+
+        {:error, error} when ipv6 == true ->
+          if check_ipv6_support() do
+            case error do
+              err when err in [:nxdomain, :ehostunreach] ->
+                {:error,
+                 Error.validation(
+                   summary:
+                     "The IPv6 host is not reachable (#{inspect(error)}). Your system seems to support IPv6, but your ISP or network may not. Consider selecting an IPv4 host, or using an IPv4-to-IPv6 tunnel.",
+                   code: error
+                 )}
+
+              :econnrefused ->
+                {:error,
+                 Error.validation(
+                   summary: "The IPv6 host is not reachable on that port (econnrefused).",
+                   code: :econnrefused
+                 )}
+
+              :timeout ->
+                {:error, Error.validation(summary: "Timed out attempting to reach the IPv6 host.", code: :timeout)}
+
+              error ->
+                {:error,
+                 Error.validation(summary: "Unknown error connecting to IPv6 host: #{inspect(error)}", code: error)}
+            end
+          else
+            {:error,
+             Error.validation(
+               summary:
+                 "Your system does not seem to support IPv6. Consider selecting an IPv4 host, or using an IPv4-to-IPv6 tunnel.",
+               code: :no_ipv6_support
+             )}
+          end
 
         {:error, error} ->
           case error do
@@ -45,7 +79,7 @@ defmodule Sequin.NetworkUtils do
               {:error, Error.validation(summary: "Timed out attempting to reach the host on that port.", code: :timeout)}
 
             error ->
-              {:error, Error.validation(summary: "Unknown error connecting to host: #{inspect(error)}")}
+              {:error, Error.validation(summary: "Unknown error connecting to host: #{inspect(error)}", code: error)}
           end
       end
     end
@@ -68,6 +102,22 @@ defmodule Sequin.NetworkUtils do
 
   defp ipv6_opts(false), do: []
   defp ipv6_opts(true), do: [:inet6]
+
+  def check_ipv6_support do
+    case :inet.getifaddrs() do
+      {:ok, ifs} ->
+        Enum.any?(ifs, fn {_ifname, attrs} ->
+          attrs
+          |> Keyword.get_values(:addr)
+          |> Enum.any?(fn addr ->
+            is_tuple(addr) and tuple_size(addr) == 8
+          end)
+        end)
+
+      {:error, _reason} ->
+        false
+    end
+  end
 
   @doc """
   Measures average TCP latency to a given endpoint.

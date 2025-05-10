f857be3fd287ae36be0c5278d139aecd74411248
David Gu <david.gurquiza@gmail.com>
2025-05-09 22:14:23 -0600

    feat: add tooltip for group message cards for existing sinks

---
+++ assets/svelte/components/ui/card/expandable-card-header.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { CardHeader, CardTitle } from ".";
+  import { ChevronDown } from "lucide-svelte";
+  import { Button as ButtonPrimitive } from "bits-ui";
+
+  export let expanded: boolean = false;
+  export let disabled: boolean = false;
+  export let builders: $$Props["builders"] = [];
+
+  function handleClick() {
+    if (disabled) return;
+    expanded = !expanded;
+  }
+</script>
+
+<CardHeader class="flex flex-row items-center justify-between">
+  <ButtonPrimitive.Root
+    class="flex flex-row items-center justify-between w-full cursor-pointer"
+    on:click={handleClick}
+    {builders}
+    {disabled}
+  >
+    <CardTitle class="flex items-center gap-2">
+      <slot />
+    </CardTitle>
+    <div
+      class="transition-transform duration-200"
+      class:rotate-180={expanded}
+      class:text-gray-300={disabled}
+    >
+      <ChevronDown class="h-4 w-4" />
+    </div>
+  </ButtonPrimitive.Root>
+</CardHeader>
--- assets/svelte/consumers/GroupColumnsForm.svelte
+++ assets/svelte/consumers/GroupColumnsForm.svelte
@@ -65,7 +65,10 @@
 
 <!-- Edit Mode Card -->
 {#if isEditMode}
-  <ExpandableCard disabled={true}>
+  <ExpandableCard
+    disabled={true}
+    titleTooltip="Message grouping can’t be changed for an active sink."
+  >
     <svelte:fragment slot="title">
       <CardTitle>Message grouping</CardTitle>
     </svelte:fragment>
@@ -87,7 +90,7 @@
 
   <!-- Create Mode - No PKs Available -->
 {:else if selectedTable && defaultGroupColumns.length === 0}
-  <ExpandableCard>
+  <ExpandableCard expanded={!isEditMode}>
     <svelte:fragment slot="title">
       <CardTitle>Message grouping</CardTitle>
     </svelte:fragment>

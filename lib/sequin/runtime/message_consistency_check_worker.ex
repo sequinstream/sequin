f7f6c3a88bdc14322f4ecbf56f35a1de2127847f
RTLS <carter.e.pedersen@gmail.com>
2025-06-16 17:21:30 -0700

    🧠 Reduce memory usage of consistency check worker

--- lib/sequin/runtime/message_consistency_check_worker.ex
+++ lib/sequin/runtime/message_consistency_check_worker.ex
@@ -23,41 +23,15 @@ defmodule Sequin.Runtime.MessageConsistencyCheckWorker do
   end
 
   def audit_and_trim_undelivered_cursors(consumer_id, older_than_timestamp) do
-    case MessageLedgers.list_undelivered_wal_cursors(consumer_id, older_than_timestamp) do
-      {:ok, []} ->
+    case MessageLedgers.count_undelivered_wal_cursors(consumer_id, older_than_timestamp) do
+      {:ok, 0} ->
         :ok
 
-      {:ok, undelivered_cursors} ->
-        Logger.info("[MessageConsistencyCheckWorker] Found undelivered cursors (count=#{length(undelivered_cursors)})",
+      {:ok, undelivered_cursor_count} ->
+        Logger.warning("[MessageConsistencyCheckWorker] Found undelivered cursors (count=#{undelivered_cursor_count})",
           consumer_id: consumer_id,
-          undelivered_cursors: inspect(undelivered_cursors)
+          undelivered_cursor_count: undelivered_cursor_count
         )
-
-        consumer = Consumers.get_sink_consumer!(consumer_id)
-
-        persisted_messages =
-          undelivered_cursors
-          # In case there are enough undelivered cursors to violate the 65535 limit on postgres params
-          |> Enum.chunk_every(10_000)
-          |> Enum.flat_map(fn chunk ->
-            Consumers.list_consumer_messages_for_consumer(consumer, wal_cursor_in: chunk)
-          end)
-
-        persisted_wal_cursor_set = MapSet.new(persisted_messages, &{&1.commit_lsn, &1.commit_idx})
-
-        undelivered_and_unpersisted_cursors =
-          Enum.reject(undelivered_cursors, fn %{commit_lsn: commit_lsn, commit_idx: commit_idx} ->
-            MapSet.member?(persisted_wal_cursor_set, {commit_lsn, commit_idx})
-          end)
-
-        unless undelivered_and_unpersisted_cursors == [] do
-          Logger.warning(
-            "[MessageConsistencyCheckWorker] Found undelivered/unpersisted cursors (count=#{length(undelivered_and_unpersisted_cursors)})",
-            consumer_id: consumer_id,
-            count: length(undelivered_and_unpersisted_cursors),
-            undelivered_cursors: inspect(undelivered_and_unpersisted_cursors)
-          )
-        end
 
         MessageLedgers.trim_stale_undelivered_wal_cursors(consumer_id, older_than_timestamp)
     end

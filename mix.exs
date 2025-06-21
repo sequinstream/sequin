9f9c1ca2f8f2881a697942a060deaab194b17e40
Anthony Accomazzo <accomazz@gmail.com>
2025-06-21 14:00:44 -0700

    [slot_producer] checkpoint 5

--- mix.exs
+++ mix.exs
@@ -133,6 +133,8 @@ defmodule Sequin.MixProject do
       {:floki, ">= 0.30.0", only: :test},
       {:uuid, "~> 1.1"},
       {:tidewave, "~> 0.1", only: :dev},
+      # Need in :dev for formatter
+      {:assert_eventually, "~> 1.0", only: [:dev, :test]},
 
       # MiniElixir extras
       {:eden, "~> 2.1.0"}

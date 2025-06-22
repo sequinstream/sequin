9073f4fedb39f01988ac65d2f989f5ed58e66222
Anthony Accomazzo <accomazz@gmail.com>
2025-06-22 08:53:22 -0700

    :sparkles: Add support for UUID in transforms

--- lib/sequin/functions/minielixir/validator.ex
+++ lib/sequin/functions/minielixir/validator.ex
@@ -302,6 +302,7 @@ defmodule Sequin.Functions.MiniElixir.Validator do
   defp fnok([Decimal, _]), do: :ok
   defp fnok([URI, _]), do: :ok
   defp fnok([Base, _]), do: :ok
+  defp fnok([UUID, _]), do: :ok
   defp fnok(p), do: {:error, :validator, "Forbidden function: #{redot(p)}"}
 
   # Convert left-associated instances of the . operator to a get_in path

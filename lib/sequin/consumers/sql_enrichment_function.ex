6d7d16b89fa6f7dac7efc137bfbd1bbf687c2b4f
RTLS <carter.e.pedersen@gmail.com>
2025-07-17 16:14:57 -0700

    🐛 Gate enrichment functions with :function_transforms

--- lib/sequin/consumers/sql_enrichment_function.ex
+++ lib/sequin/consumers/sql_enrichment_function.ex
@@ -16,7 +16,7 @@ defmodule Sequin.Consumers.SqlEnrichmentFunction do
   def changeset(struct, params, account_id) do
     changeset = cast(struct, params, [:code])
 
-    if Sequin.feature_enabled?(account_id, :enrichment_functions) do
+    if Sequin.feature_enabled?(account_id, :function_transforms) do
       validate_required(changeset, [:code])
     else
       add_error(changeset, :type, "Enrichment functions are not enabled. Talk to the Sequin team to enable them.")

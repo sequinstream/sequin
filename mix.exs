534e1e75ca8eb71ad4d5fe7feacfc2c7ea73d765
davo <davoclavo@users.noreply.github.com>
2025-07-21 15:28:23 -0600

    🛠️ Remove `test/support` from being compiled in :dev mode (#2030)

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Sequin.MixProject do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(:dev), do: ["lib", "test/support", "bench"]
+  defp elixirc_paths(:dev), do: ["lib", "bench"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.

187b48c757c94cb950f12b963bb99f8820d255e7
Yordis Prieto <yordis.prieto@gmail.com>
2025-08-06 14:13:21 -0400

    feat: define mysql_module in Sequin.Sinks.Mysql for improved configuration

--- lib/sequin/sinks/mysql.ex
+++ lib/sequin/sinks/mysql.ex
@@ -7,22 +7,20 @@ defmodule Sequin.Sinks.Mysql do
   @callback upsert_records(MysqlSink.t(), [map()]) :: :ok | {:error, Error.t()}
   @callback delete_records(MysqlSink.t(), [any()]) :: :ok | {:error, Error.t()}
 
+  @module Application.compiled_env(:sequin, :mysql_module, Sequin.Sinks.Mysql.Client)
+
   @spec test_connection(MysqlSink.t()) :: :ok | {:error, Error.t()}
   def test_connection(%MysqlSink{} = sink) do
-    impl().test_connection(sink)
+    @module.test_connection(sink)
   end
 
   @spec upsert_records(MysqlSink.t(), [map()]) :: :ok | {:error, Error.t()}
   def upsert_records(%MysqlSink{} = sink, records) when is_list(records) do
-    impl().upsert_records(sink, records)
+    @module.upsert_records(sink, records)
   end
 
   @spec delete_records(MysqlSink.t(), [any()]) :: :ok | {:error, Error.t()}
   def delete_records(%MysqlSink{} = sink, record_pks) when is_list(record_pks) do
-    impl().delete_records(sink, record_pks)
-  end
-
-  defp impl do
-    Application.get_env(:sequin, :mysql_module, Sequin.Sinks.Mysql.Client)
+    @module.delete_records(sink, record_pks)
   end
 end
